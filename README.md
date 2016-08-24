a1d23345403bb80e04ae37e5eb14c96a46e06cfb
Mauricio nunes <Mauricio nunes>
2016-08-24 10:42:00 -0300

    up/working

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-## Twitter about sharepoint saved in a sharepoint list
+## Twit's about sharepoint saved in a sharepoint list
+
+1. Register twitter app to get  consumer_key, consumer_secret, access_token, access_token_secret
+2. Register the add-in in the site you want to read's appregnew.aspx page. http://tenant.sharepoint.com/_layouts/appregnew.aspx
+3. Go to the appinv page in your admin site and grant the permissions http://tenant-admin.sharepoint.com/_layouts/appinv.aspx
+ ```xml
+ <AppPermissionRequests AllowAppOnlyPolicy="true">
+    <AppPermissionRequest Scope="http://sharepoint/content/sitecollection" Right="FullControl" />
+    <AppPermissionRequest Scope="http://sharepoint/social/tenant" Right="FullControl" />
+    <AppPermissionRequest Scope="http://sharepoint/search" Right="QueryAsUserIgnoreAppPrincipal" />
+  </AppPermissionRequests>
+```
+
+4. Go back to the site you want to read and register the below permissions in that site's http://tenant.sharepoint.com/_layouts/appinv.aspx: **appinv.aspx**
+  ```xml
+  <AppPermissionRequests AllowAppOnlyPolicy="true" >
+    <AppPermissionRequest Scope="http://sharepoint/content/sitecollection" Right="FullControl" />
+  </AppPermissionRequests>
+  ```
+5. fork this repo
+6. clone them
+7. Install dependencies with `npm install`
+8. Copy and paste the `config.sample.js`  and change to `config.js` after that add the necessary info to work with bot services
+9. Start dev with `npm run dev`
+
+
+Obs: if you don't have one sharepoint dev tenant check out this [link](https://profile.microsoft.com/RegSysProfileCenter/wizardnp.aspx?wizid=14b845d0-938c-45af-b061-f798fbb4d170&lcid=1033)
--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 'use strict';
 
 const config = require('./config');
-const twit = require('twit');
+const Twit = require('twit');
 const pnp = require('sp-pnp-js');
 const T = new Twit(config.twitter);
 
-const initialize = () => {
-    console.log('Iniciando...')
-    
-    pnp.setup(config.sp);
+const spConfig = {
+    list: 'Twits',
+    tags: ['#sharepoint', '#PnP-JS-Core', '#SPFX', '#Office365']
+}
 
-    let stream = T.stream('statuses/filter', { track: '#apple', language: 'en' })
+const initializeList = () => {
+    console.log('Iniciando...');
+    pnp.sp.web.lists.ensure(spConfig.list)
+        .then(response => {
+            console.log(response.data.Title);
+            initializeTwit();
+        })
+        .catch(error => {
+            console.log(Error(error));
+        });
+}
 
+const initializeTwit = () => {
+    console.log('Iniciando Twit...');
+    let stream = T.stream('statuses/filter', { track: spConfig.tags });
     stream.on('tweet', function (tweet) {
-        console.log(tweet)
+        console.log('Saving twit :', tweet.text);
+        pnp.sp.web.lists.getByTitle(spConfig.list).items
+            .add({
+                Title: tweet.text
+            })
+            .then(response => {
+                console.log('Item created :', response.data.Title);
+            })
+            .catch(error => {
+                console.log(Error(error));
+            });
     });
 }
+
+const initialize = () => {
+    pnp.setup(config.sp);
+    pnp.sp.web.select("Title").get()
+        .then((web) => {
+            console.log('We are working on :', web.Title)
+            initializeList()
+        })
+        .catch(error => {
+            console.log(Error(error));
+        });
+}
+
+initialize();
